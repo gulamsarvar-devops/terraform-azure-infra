4f24ee7a8f7927ac219e7023290911822ab752fd
Gulam Sarvar <103048640+gulamsarvar-devops@users.noreply.github.com>
2026-05-10 15:42:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "devopsstorage12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
